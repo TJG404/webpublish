3938fa1de2f7d8d99a0e800cc5a94810c491a7a9
lee <lkm1401@naver.com>
2025-09-01 16:33:44 +0900

    2025-09-01 commit~

---
+++ JavaScript/8. array/3_forEach.js
@@ -0,0 +1,16 @@
+/**
+ * forEach 메소드는 Iterator 심볼 객체를 상속한 객체에서 제공됨
+ * - 배열.forEach(callbackFn);
+ */
+let fruits = new Array('🍎', '🍊', '🍋', '🍏', '🍑');
+fruits.forEach((item) => console.log(item));
+
+//🍋을 🍇도 교체
+fruits.forEach((item, index) => {
+    if(item === '🍋') {
+        fruits.splice(index, 1, '🍇');
+    }
+});
+
+console.log(fruits);
+
