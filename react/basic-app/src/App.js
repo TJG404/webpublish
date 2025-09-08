e6d6d7bbbff720f3e656e3214f74016410e3a102
lee <lkm1401@gmail.com>
2025-09-08 12:45:55 +0900

    2025-09-08 commit

---
+++ react/basic-app/src/App.js
@@ -0,0 +1,44 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+   <div>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+    <h1 className="App">Hello~ React App!!!</h1>
+   </div>
+  );
+}
+
+export default App;
