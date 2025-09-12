122a0cad4f83e1fa405e4b53cc3183ac8eafd49e
lee <lkm1401@gmail.com>
2025-09-12 12:45:35 +0900

    2025-09-12 commit

--- react/basic-app/src/AppMenu.js
+++ react/basic-app/src/AppMenu.js
@@ -16,15 +16,22 @@ export default function App() {
     }, []);
 
     return (
-        <div style={{display:"flex", 
-                    justifyContent:"space-between",
-                    borderBottom: "1px solid #888",
-                    padding: "30px"}}>
-            <MenuList menus={menus.headerLeft}/>
-            <Logo img="/images/logoRed.png" alt="header-logo" w="120px" />
-            <MenuList menus={menus.headerRight}/>
-            {/* <MenuList menus="headerRight"/>
-            <MenuList menus="headerCenter"/> */}
+        <div>
+            <div style={{display:"flex", 
+                        justifyContent:"space-between",
+                        borderBottom: "1px solid #888",
+                        padding: "20px"}}>
+                <MenuList menus={menus.headerLeft}/>
+                <Logo img="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/250px-Google_2015_logo.svg.png" 
+                    alt="header-logo" w="200px" />
+                <MenuList menus={menus.headerRight}/>
+            </div>
+            <div style={{display:"flex", justifyContent:"center"}}>
+                <MenuList menus={menus.headerCenter}/>
+            </div>
+            <div style={{display:"flex", justifyContent:"center"}}>
+                <MenuList menus={menus.footerTop}/>
+            </div>
         </div>
     );
 }
