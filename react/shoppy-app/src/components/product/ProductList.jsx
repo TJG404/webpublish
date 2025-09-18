47f22a600746d03e7a9a380f9831c0de576b2f8b
lee <lkm1401@gmail.com>
2025-09-18 14:28:46 +0900

    025-09-18 commit

--- react/shoppy-app/src/components/product/ProductList.jsx
+++ react/shoppy-app/src/components/product/ProductList.jsx
@@ -14,17 +14,23 @@ export function ProductList() {
     }, []);
    
     //출력 포맷 함수 : 한줄에 상품 3개씩 출력
-    const rows = [];  // [ [{}, {}, {}], [{}, {}, {}], [{}] ]
-    for(let i=0; i<list.length; i+=3) {
-        rows.push(list.slice(i, i+3)); //0~2, slice 새로운 배열 반환
-    }
+    // const rows = [];  // [ [{}, {}, {}], [{}, {}, {}], [{}] ]
+    // for(let i=0; i<list.length; i+=3) {
+    //     rows.push(list.slice(i, i+3)); //0~2, slice 새로운 배열 반환
+    // }
+
+    const rows = list.reduce((acc, cur, idx) => {
+        if(idx % 3 === 0) acc.push([cur])
+        else acc[acc.length-1].push(cur);
+        return acc;
+    }, []);
 
     return (
         <div>
-                {rows && rows.map(rowArray => 
-                    <div className='product-list'>
-                        {rowArray && rowArray.map(product => 
-                            <ProductAvatar img={product.image} />
+                {rows && rows.map((rowArray, idx) => 
+                    <div className='product-list' key={idx} >
+                        {rowArray && rowArray.map((product, idx) => 
+                            <ProductAvatar img={product.image} key={idx} />                            
                         )}
                     </div>
                  )}
