9ee9103f8f12887b0abc3066e2ded910872f7d40
lee <lkm1401@gmail.com>
2025-09-22 17:04:47 +0900

    2025-09-22 commit

--- react/shoppy-app/src/components/detailTabs/Return.jsx
+++ react/shoppy-app/src/components/detailTabs/Return.jsx
@@ -18,7 +18,25 @@ export function Return() {
         <div>
             <div style={{paddingTop:"20px"}}></div>
             <h4>{returnData && returnData.title}</h4>
-            <p>{returnData && returnData.description}</p>
+            <p style={{paddingBottom:"20px"}}>{returnData && returnData.description}</p>
+            <table className='review-list-content'>
+                <tbody>
+                    {returnData.list && returnData.list.map(item => 
+                        <tr>
+                            <td style={{width:"30%", textAlign:"center"}}>{item.title}</td>
+                            <td>
+                                <ul  style={{textAlign:"left"}}>
+                                {item.infoList 
+                                    && item.infoList .map(item =>
+                                        <li>{item}</li>
+                                    )}
+                                </ul>
+                            </td>
+                        </tr>
+                    ) }
+                    <tr><td colSpan={2}></td></tr>
+                </tbody>
+            </table>
         </div>
     );
 }
