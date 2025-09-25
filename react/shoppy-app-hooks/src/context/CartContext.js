c7910b0fe747dcb0d1cdb4f269f66c0cc6ba1274
lee <lkm1401@gmail.com>
2025-09-25 12:39:02 +0900

    2025-09-25 commit

--- react/shoppy-app-hooks/src/context/CartContext.js
+++ react/shoppy-app-hooks/src/context/CartContext.js
@@ -5,7 +5,7 @@ export const CartContext = createContext();
 
 //2. CartProvider는 CartContext를 사용하는 컴포넌트들의 범위를 정의
 export const CartProvider = ({children}) => {
-    const [cartCount, setCartCount] = useState(999);  //1. 장바구니 수량 관리 
+    const [cartCount, setCartCount] = useState(0);  //1. 장바구니 수량 관리 
     const [cartList, setCartList] = useState([]);
     
     return (
--- react/shoppy-app-hooks/src/pages/Cart.jsx
+++ react/shoppy-app-hooks/src/pages/Cart.jsx
@@ -1,51 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { axiosData } from '../utils/dataFetch.js';
 import { cartItemsAddInfo, getTotalPrice } from '../utils/cart.js';
+import { CartContext } from '../context/CartContext.js';
+import { useCart } from '../hooks/useCart.js';
 import '../styles/cart.css';
 
-export function Cart({ items, updateCart }) {
+export function Cart({ updateCart }) {
+    const { showCart } = useCart();
+    const { cartList } = useContext(CartContext);
+
     const navigate = useNavigate();
-    const [cartList, setCartList] = useState([]);
+    // const [cartList, setCartList] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0);   
     
     useEffect(()=> {
-        const fetch = async() => {
-            const jsonData = await axiosData("/data/products.json");
-            setCartList(cartItemsAddInfo(jsonData, items));
-            setTotalPrice(getTotalPrice(jsonData, items));
-        }
-        fetch();
+        showCart();
+        // const fetch = async() => {
+        //     const jsonData = await axiosData("/data/products.json");
+        //     setCartList(cartItemsAddInfo(jsonData, items));
+        //     // setTotalPrice(getTotalPrice(jsonData, items));
+        // }
+        // fetch();
     }, []);    
 
     //수량 업데이트 함수
-    const handleUpdateCartList = (cid, type) => {
-        setCartList((cartList) => cartList.map((item) => 
-                item.cid === cid ?
-                    type === '+'? {...item, qty: item.qty+1}   
-                                : item.qty > 1 ? {...item, qty: item.qty-1} : item
-                :   item  
-        ));
+    // const handleUpdateCartList = (cid, type) => {
+    //     setCartList((cartList) => cartList.map((item) => 
+    //             item.cid === cid ?
+    //                 type === '+'? {...item, qty: item.qty+1}   
+    //                             : item.qty > 1 ? {...item, qty: item.qty-1} : item
+    //             :   item  
+    //     ));
 
-        const findItem = cartList.find((item) => item.cid === cid);
-        type === '+'?  setTotalPrice(totalPrice + findItem.price) 
-                        : findItem.qty > 1 ? setTotalPrice(totalPrice-findItem.price)
-                                        : setTotalPrice(totalPrice);
-        updateCart(cid, type);
-    }
+    //     const findItem = cartList.find((item) => item.cid === cid);
+    //     type === '+'?  setTotalPrice(totalPrice + findItem.price) 
+    //                     : findItem.qty > 1 ? setTotalPrice(totalPrice-findItem.price)
+    //                                     : setTotalPrice(totalPrice);
+    //     updateCart(cid, type);
+    // }
 
     //장바구니 아이템 삭제 함수
-    const handleRemoveCartList = (cid) => {
-        const findItem = cartList.find(item => item.cid === cid);
-        setTotalPrice(totalPrice - (findItem.qty * findItem.price));
+    // const handleRemoveCartList = (cid) => {
+    //     const findItem = cartList.find(item => item.cid === cid);
+    //     setTotalPrice(totalPrice - (findItem.qty * findItem.price));
 
-        setCartList((cartList) => {
-            return cartList.filter(item => !(item.cid === cid));  
-        });
+    //     setCartList((cartList) => {
+    //         return cartList.filter(item => !(item.cid === cid));  
+    //     });
 
-        updateCart(cid);
-    }
+    //     updateCart(cid);
+    // }
 
     return (
         <div className='cart-container'>
@@ -61,23 +67,23 @@ export function Cart({ items, updateCart }) {
                                 {parseInt(item.price).toLocaleString()}원</p>
                         </div>
                         <div className='cart-quantity'>
-                            <button type='button'
+                            {/* <button type='button'
                                     onClick={()=>{ item.qty > 1 &&
-                                                    handleUpdateCartList(item.cid, '-')}}>-</button>
+                                                    handleUpdateCartList(item.cid, '-')}}>-</button> */}
                             <input type='text' value={item.qty} readOnly/>
-                            <button type='button'
-                                    onClick={()=>{handleUpdateCartList(item.cid, '+')}}>+</button>
+                            {/* <button type='button'
+                                    onClick={()=>{handleUpdateCartList(item.cid, '+')}}>+</button> */}
                         </div>
-                        <button className='cart-remove'
-                                onClick={()=>{handleRemoveCartList(item.cid)}}>
+                        {/* <button className='cart-remove'
+                                onClick={()=>{handleRemoveCartList(item.cid)}}> 
                             <RiDeleteBin6Line />
-                        </button>
+                        </button> */}
                     </div>
                 </div>    
             )}
 
             {/* 주문 버튼 출력 */}
-            { cartList && cartList.length > 0 ?
+            {/* { cartList && cartList.length > 0 ?
                 <>
                     <div className='cart-summary'>
                         <h3>주문 예상 금액</h3>
@@ -115,7 +121,7 @@ export function Cart({ items, updateCart }) {
                     <img src="/images/cart.jpg" 
                          style={{width:"50%", marginTop:"20px"}} />
                 </div>
-            }
+            } */}
         </div>
     );
 }
