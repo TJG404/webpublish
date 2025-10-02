a3c276a6d640756d90d8da48793d2b0836f55a81
lee <lkm1401@gmail.com>
2025-10-02 10:50:17 +0900

    2025-10-02 commit

--- react/shoppy-app-redux/src/app/store.js
+++ react/shoppy-app-redux/src/app/store.js
@@ -3,10 +3,22 @@ import cartSlice  from '../feature/cart/cartSlice.js'
 import productSlice  from '../feature/product/productSlice.js'
 import authSlice from '../feature/auth/authSlice.js'
 
+//액션 로깅 처리 담당 미들웨어
+const myLoggerMiddlware = (store) => (next) => (action) => {
+  console.log("dispatch :: ", action);
+  const result = next(action);
+  console.log("next state :: ", store.getState());  
+
+  return result;
+}
+
 export const store = configureStore({
   reducer: {
     cart: cartSlice,
     product: productSlice,
     auth: authSlice
   },
+  middleware: (getDefaultMiddleware) =>
+                getDefaultMiddleware()
+                .concat(myLoggerMiddlware) 
 })
